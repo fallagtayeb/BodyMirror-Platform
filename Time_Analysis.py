7a85c7640f19879da94b0f2e7feb58b1302af78c
Myelin-H <108212577+Myelin-H@users.noreply.github.com>
2022-07-26 20:42:03 +0100

    Update Time_Analysis.py

--- Time_Analysis.py
+++ Time_Analysis.py
@@ -126,7 +126,7 @@ def time_analysis(epochs_p):
         #anim_c3.save(r'Animation1.mp4')
         #HtmlFile = line_ani.to_html5_video()
         with open("myvideo.html","w") as f:
-          anim_c3.to_html5_video()
+          st.write(anim_c3.to_html5_video(), file=f)
             #print(anim_c3.to_html5_video(), file=f)
 
         HtmlFile = open("myvideo.html", "r")
