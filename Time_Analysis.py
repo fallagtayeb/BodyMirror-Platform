1994ae7ee10e7d58235bc6a75196da3667b15d66
Myelin-H <108212577+Myelin-H@users.noreply.github.com>
2022-07-26 22:54:18 +0100

    Update Time_Analysis.py

--- Time_Analysis.py
+++ Time_Analysis.py
@@ -91,7 +91,7 @@ import streamlit.components.v1 as components
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-!pip install ffmpeg
+from IPython.display import HTML
 
 #import myelinh_functions
 #import text_to_speech
